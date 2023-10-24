9d880877869f85fdf07a9032bf7c35cd4b5726c4
Deividi Paganini <deividi.paganini@dnx.solutions>
2023-10-24 18:27:31 +1030

    fix ip_set name

--- ip-set.tf
+++ ip-set.tf
@@ -1,7 +1,7 @@
 resource "aws_wafv2_ip_set" "ip_set" {
   for_each = { for rule in try(var.ip_set_reference_statement_rules, []) : rule.name => rule if length(try(rule.ip_set, [])) > 0 }
 
-  name               = "waf-${each.value.name}"
+  name               = each.value.name
   ip_address_version = "IPV4"
   addresses          = each.value.ip_set
   scope              = var.scope
